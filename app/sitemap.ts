b46bd611490d515cebe3cc8d271a79ae0eea5001
Pawankp-Dev-Web <pawan.prajapati@saasho.com>
2026-09-08 04:51:54 +0530

    done

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -48,6 +48,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     {
+      url: `${baseUrl}/faqs`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/contact-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
